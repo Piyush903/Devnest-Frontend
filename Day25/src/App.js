e4d0c4f141ec9579a9b1bb9f27a574f5a07dea6b
Piyush903 <84367647+Piyush903@users.noreply.github.com>
2021-07-30 19:05:11 +0530

    day 25 added

---
+++ Day25/src/App.js
@@ -0,0 +1,14 @@
+import './styles/output.css'
+import NavBarComponent from './components/NavBar';
+import Carousel from './components/Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarComponent/>
+      <Carousel/>
+    </div>
+  );
+}
+
+export default App;
