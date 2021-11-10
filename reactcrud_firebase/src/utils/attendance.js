202786b98bddc76255736e6440a6214ed044ada5
Piyush Nijhawan <piyushnijhawan78@gmail.com>
2021-11-10 21:31:21 +0530

    updates are down

--- reactcrud_firebase/src/utils/attendance.js
+++
@@ -1,7 +0,0 @@
-import { database } from "../firebase";
-
-export const getAttendanceObjects = async () => {
-  const snapshot = await database.ref("/Attendance").once("value");
-  const data = snapshot.val() || {};
-  return data;
-};
